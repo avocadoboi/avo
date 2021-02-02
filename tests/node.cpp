8c3674798553f92160daf5968e81130fb671b216
Björn Sundin <bjornsundin02@gmail.com>
2021-02-02 12:50:46 +0100

    make node test file

--- tests/node.cpp
+++ tests/node.cpp
@@ -1,15 +1,14 @@
 #include "testing_header.hpp"
 
-class App : avo::NodeTag {
+class App {
 public:
 
-    
 private:
-    avo::Node _node;
+    avo::Node _node{this};
 };
 
 TEST_CASE("Nodes with IDs") {
     auto root = avo::Node{};
 
-    root.add_child()
+    root.add_child();
 }
